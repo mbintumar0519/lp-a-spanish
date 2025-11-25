53bb872c2c3113756bd7bf35596db17ce9242a1b
maryambintumar <mbintumar@ufl.ed>
2025-11-25 14:47:16 -0500

    Update Google Tag Manager ID to GTM-PWC5KMWV

--- app/layout.js
+++ app/layout.js
@@ -86,17 +86,11 @@ export default function RootLayout({ children }) {
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `(
-              function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              }
-            )(window,document,'script','dataLayer','GTM-MXW78KVC');`
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PWC5KMWV');`
           }}
         />
         <link rel="icon" href="/icon.png?v=2" type="image/png" />
@@ -172,7 +166,7 @@ export default function RootLayout({ children }) {
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MXW78KVC"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PWC5KMWV"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
